0392c9fe1d4f5b701cca28199e9cad51b02d3bcb
Mohammad El-Abid <mohammad.elabid@gmail.com>
2011-03-04 09:39:04 -0500

    Rails 3.0.5 base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookPageSpamCheck::Application.config.secret_token = 'aeae12ec0e62e46644e39dd4f1a24e88b5c70bd4a8be5669bc648bcd12bf4969053e6d697b9a4007f97bba3fbfa359a1aa21eb68dcbde4cb9f0a2c5086be3c37'
