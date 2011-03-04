1bf4f2eef8c5ab8cf834ff69f663178d3365c0a6
Mohammad El-Abid <mohammad.elabid@gmail.com>
2011-03-04 16:44:33 -0500

    Adds a lot of functionality

--- app/controllers/search_controller_controller.rb
+++ app/controllers/search_controller_controller.rb
@@ -1,5 +1,3 @@
-# TODO: check the indention. Had to switch IDEs.
-
 require 'open-uri'
 
 class SearchControllerController < ApplicationController
@@ -7,25 +5,35 @@ class SearchControllerController < ApplicationController
   end
   
   def show
-	# TODO: SearchController.show
-    @json = ActiveSupport::JSON.decode(open(nil).read)['data']
+	  # TODO: SearchController.show
+    @json = ActiveSupport::JSON.decode(open("http://graph.facebook.com/#{params[:id]}/feed").read)['data']
+    @page = ActiveSupport::JSON.decode(open("http://graph.facebook.com/#{params[:id]}").read)
+    @page['linked_name'] = "<a href='#{@page['link']}' target='_blank' title='#{@page['name']}'>#{@page['name']}</a>"
   end
 
   def search
-    @search[:page]   = params[:page] || 1
-	@search[:limit]  = 25
-	@search[:offset] = (@search[:page]-1) * @search[:limit]
+    if not params[:page].nil?
+      params[:page] = params[:page].to_i
+      if 1 > params[:page]
+        params[:page] = nil
+      end
+    end
+    
+    @search          = Hash.new
+    @search[:page]   = params[:page]|| 1
+	  @search[:limit]  = 25
+	  @search[:offset] = (@search[:page]-1) * @search[:limit]
 	
     # JSON.decode(open(url).read)['data']
-    @json = ActiveSupport::JSON.decode(open("https://graph.facebook.com/search?q=#{params[:q]}&type=page&limit=#{@search[:limit]+1}&offset=#{@search[:offset]}").read)['data']
-	# in the query above, we pull 26 results, however we are only going to show 25.
-	# but if there is a 26th, then we know there is a next page.
-	if @json.length == (@search[:limit] + 1)
-	  @search[:more] = true
-	  @json.pop # remove the last element
-	else
-	  @search[:more] = false
-	end
+    @json = ActiveSupport::JSON.decode(open("http://graph.facebook.com/search?q=#{params[:q]}&type=page&limit=#{@search[:limit]+1}&offset=#{@search[:offset]}").read)['data']
+	  # in the query above, we pull 26 results, however we are only going to show 25.
+	  # but if there is a 26th, then we know there is a next page.
+	  if @json.length == (@search[:limit] + 1)
+	    @search[:more] = true
+	    @json.pop # remove the last element
+	  else
+	    @search[:more] = false
+	  end
 	
     respond_to do |format|
       format.html 
